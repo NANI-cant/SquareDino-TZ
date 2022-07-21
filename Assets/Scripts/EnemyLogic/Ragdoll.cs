1a24d4acbde222d8c851b75ce2d8bdd7af82f5e4
NANI-cant <trixxx.asd@yandex.ru>
2022-07-21 20:24:16 +0300

    ragdoll fix

--- Assets/Scripts/EnemyLogic/Ragdoll.cs
+++ Assets/Scripts/EnemyLogic/Ragdoll.cs
@@ -1,58 +1,41 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Ragdoll : MonoBehaviour {
     private Rigidbody[] _bones;
-
-    private Vector3[] _savedBonesPositions;
-    private Quaternion[] _savedBonesRotations;
+    private List<Collider> _colliders = new List<Collider>();
 
     private void Awake() {
         _bones = GetComponentsInChildren<Rigidbody>();
-        SaveBones();
-    }
 
-    public void Fall() {
-        SaveBones();
-        Enable();
-    }
+        foreach (var bone in _bones) {
+            if (bone.TryGetComponent<Collider>(out Collider collider)) {
+                _colliders.Add(collider);
+            }
+        }
 
-    public void StandUp() {
-        LoadBones();
         Disable();
     }
 
-    private void SaveBones() {
-        _savedBonesPositions = new Vector3[_bones.Length];
-        _savedBonesRotations = new Quaternion[_bones.Length];
-
-        for (int i = 0; i < _bones.Length; i++) {
-            _savedBonesPositions[i] = _bones[i].transform.position;
-            _savedBonesRotations[i] = _bones[i].transform.rotation;
-        }
+    public void Fall() {
+        Enable();
     }
-
-    private void LoadBones() {
-        if (_savedBonesPositions.Length != _bones.Length || _savedBonesRotations.Length != _bones.Length) {
-            Debug.LogError($"{gameObject.name}: can't load bones, arrays don't match");
-            return;
-        }
-
-        for (int i = 0; i < _bones.Length; i++) {
-            _savedBonesPositions[i] = _bones[i].transform.position;
-            _savedBonesRotations[i] = _bones[i].transform.rotation;
-        }
-    }
-
 
     private void Enable() {
         foreach (var bone in _bones) {
             bone.isKinematic = false;
+        }
+        foreach (var collider in _colliders) {
+            collider.enabled = true;
         }
     }
 
     private void Disable() {
         foreach (var bone in _bones) {
             bone.isKinematic = true;
+        }
+        foreach (var collider in _colliders) {
+            collider.enabled = false;
         }
     }
 }
